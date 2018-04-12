b7e014ecd5249cc8d960b9e437b06bc33f9f1903
Duncan <Duncan.David@gmail.com>
2018-04-11 19:12:40 -0700

    add findDeepAll and test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const _ = require('lodash');
-
 const defPredicate = (match, key) => (a) => a[key] === match;
-const defConfig = {
-  shouldRecurse: a => a.children,
-  childNodes: 'children'
-};
 const toggleUpdate = (toggleKey) => (a) => ({...a, [toggleKey]: !a[toggleKey]});
 const updateChildren = (key, val) => (a) => (a.children ?
   { ...a, children: a.children.map(ac => ({...ac, [key]: val}))} : a);
 const setOne = (key, val) => (a) => ({...a, [key]: val});
+
+const defConfig = {
+  shouldRecurse: a => a.children,
+  childNodes: 'children'
+};
 
 const findDeepAnd = ({array, predicate, update, config = defConfig}) => {
   return array.map(a => {
@@ -25,4 +25,17 @@ const findDeepAnd = ({array, predicate, update, config = defConfig}) => {
   });
 };
 
-module.exports = {findDeepAnd, toggleUpdate, updateChildren, defPredicate, setOne};
+const findDeepAll = ({array, predicate, config = defConfig}) => {
+  return array.reduce((av, cv) => {
+    if (predicate(cv)) {
+      av.push(cv);
+    }
+
+    if (config.shouldRecurse(cv)) {
+      return av.concat(findDeepAll({array: cv[config.childNodes], predicate, config}));
+    }
+    return av;
+  }, []);
+};
+
+module.exports = {findDeepAnd, findDeepAll, toggleUpdate, updateChildren, defPredicate, setOne};
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,24 +1,35 @@
 /* global describe, it, expect, test */
-const {findDeepAnd, toggleUpdate, updateChildren, setOne, defPredicate} = require('./index');
+const {findDeepAnd, findDeepAll, toggleUpdate, updateChildren, setOne, defPredicate} = require('./index');
 const _ = require('lodash');
 
 const simpleArray = [{id: 'foo0', someToggle: false}, {id: 'foo1', someToggle: true}, {id: 'foo2', someToggle: false}];
 
-const nestedArray = s => ({text: s, id: _.uniqueId(), children: [
-  {text: 'child1', id: 'child1', someBoolean: false},
-  {text: 'child2', id: 'child2', someBoolean: false}
-]});
+const nestedArray = ['foo', 'pony'].map((s) => ({text: s, id: _.uniqueId(), someBoolean: true, children: [
+  {text: 'child1', id: _.uniqueId(), someBoolean: false},
+  {text: 'child2', id: _.uniqueId(), someBoolean: false}
+]}));
 
+const testArr = findDeepAnd({
+  array: simpleArray,
+  predicate: defPredicate('foo0', 'id'),
+  update: toggleUpdate('someToggle')
+});
 
 describe('findDeepAnd', () => {
 
   it('should return a new array with a toggled boolean value defined in the update that matches the predicate', () => {
-    const newArr = findDeepAnd({
-      array: simpleArray,
-      predicate: defPredicate('foo0', 'id'),
-      update: toggleUpdate('someToggle')
-    });
 
-    expect(newArr[0].someToggle).toEqual(true);
+    expect(testArr[0].someToggle).toEqual(true);
   });
 });
+
+describe('findDeepAll', () => {
+
+  const findAllFalseSomeBooleans = (array) => findDeepAll({array, predicate: defPredicate(false, 'someBoolean')});
+  it('should return all items which match the predicate in a one-dimensional array', () => {
+
+    expect(findAllFalseSomeBooleans(nestedArray).length).toEqual(4)
+  });
+});
+
+
